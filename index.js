295a94a59d3a762b7cf98654749b2bf241cb5006
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-03 10:40:43 -0400

    commit tree fd987a806a62db19278dfb054237b69f3b9c1e14

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  books.forEach (book => {
+    const bookLi = document.createElement("li")
+    bookLi.textContent = book.name
+    main.appendChild(bookLi)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
